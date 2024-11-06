926a434c8d8168f3dcbbacbbe1b6f6fe4ceb6939
Kauan Cornelsen <115079606+k1cornelsen@users.noreply.github.com>
2024-11-06 00:18:17 -0300

    Update insere-carrinho.php
    Prevenção SQL Injection Snyk

--- php/insere-carrinho.php
+++ php/insere-carrinho.php
@@ -4,6 +4,20 @@ $nome = $_POST['nome'];
 
 include "conexao.php";
 
-$sql = "INSERT INTO carrinho SELECT * FROM produto WHERE nome = '$nome'";
+// Preparar a consulta SQL para prevenir SQL Injection
+$sql = "INSERT INTO carrinho SELECT * FROM produto WHERE nome = ?";
 
-mysqli_query($con, $sql);
+// Preparar o statement
+$stmt = mysqli_prepare($con, $sql);
+
+// Vincular o parâmetro
+mysqli_stmt_bind_param($stmt, "s", $nome);
+
+// Executar o statement
+mysqli_stmt_execute($stmt);
+
+// Fechar o statement
+mysqli_stmt_close($stmt);
+
+// Fechar a conexão
+mysqli_close($con);
