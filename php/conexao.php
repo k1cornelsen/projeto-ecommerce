84d2ed53af1912cacffb499f6fb18984671be524
Your Name <you@example.com>
2024-11-10 22:59:36 +0000

    lets

--- php/conexao.php
+++ php/conexao.php
@@ -1,6 +1,5 @@
 
 <?php
-// snyk ignore php/HardcodedPassword --reason="Credenciais permitidas para desenvolvimento"
 $con = mysqli_connect("database-service", "toor", "senhasecure1234#", "projetosemestral");
 
 if (!$con) {
